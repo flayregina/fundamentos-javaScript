1dcd625ac36cdee99ab83351835a3ca1b8a050ee
Flavia Regina <flaviafreire23@gmail.com>
2022-07-31 12:04:05 -0300

    Aula IIFE -> Immediately Invoked Function Expression

---
+++ 3funcao/21_IIFE.js
@@ -0,0 +1,6 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function () {
+    console.log('It will run on time!') //Será executado na hora!
+    console.log('It escapes the broader global scope!') //Foge do escopo global mais abrangente!
+})()
