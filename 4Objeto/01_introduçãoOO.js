85b1018d546d9289e50d8641106767e0c42ff64a
Flavia Regina <flaviafreire23@gmail.com>
2022-08-03 21:03:45 -0300

    introducão a objeto

---
+++ 4Objeto/01_introduçãoOO.js
@@ -0,0 +1,16 @@
+// codigo não executada!!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento
